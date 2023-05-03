82eaa9b9fc85db2028b8d6a70e5381997f72b389
Clinton <clintonchakma.24csedu.064@gmail.com>
2023-05-03 11:43:27 +0600

    created EmployeeImage Table

---
+++ EMS/Migrations/20230503045803_AddEmployeeImages.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmployeeImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeImages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmployeeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ImageData = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeImages_AspNetUsers_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeImages_EmployeeId",
+                table: "EmployeeImages",
+                column: "EmployeeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeImages");
+        }
+    }
+}
