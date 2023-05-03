1e99d5cae97a4845969301ed75ec3e5e267f683c
Clinton <clintonchakma.24csedu.064@gmail.com>
2023-05-03 10:26:34 +0600

    Updated Employees Table in Database, set ProjectId On delete NULL Added Edit and Delete Option in Project Details View

---
+++ EMS/Migrations/20230503041540_UpdateEmployeesTable_ProjectIdOnDeleteNull.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEmployeesTable_ProjectIdOnDeleteNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE dbo.AspNetUsers DROP CONSTRAINT FK_AspNetUsers_Projects_ProjectId");
+            migrationBuilder.Sql("ALTER TABLE dbo.AspNetUsers ADD CONSTRAINT FK_AspNetUsers_Projects_ProjectId FOREIGN KEY (ProjectId) REFERENCES dbo.Projects (ProjectId) ON DELETE SET NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE dbo.AspNetUsers DROP CONSTRAINT FK_AspNetUsers_Projects_ProjectId");
+            migrationBuilder.Sql("ALTER TABLE dbo.AspNetUsers ADD CONSTRAINT FK_AspNetUsers_Projects_ProjectId FOREIGN KEY (ProjectId) REFERENCES dbo.Projects (ProjectId) ON DELETE SET NULL");
+        }
+    }
+}
